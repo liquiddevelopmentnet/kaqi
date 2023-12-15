da1ff5e06a73b507dd16010257db4e48768d83eb
Finn Behrend <76592300+liquiddevelopmentnet@users.noreply.github.com>
2023-12-15 07:51:09 +0100

    style: delete /lib directory

--- lib/decorators/sharedUtils.d.ts
+++
@@ -1,4 +0,0 @@
-import { Service } from '..';
-export declare const ensureProps: (target: Service, field: string) => void;
-export declare const buildServiceDecorator: (fields: Record<string, unknown>) => (target: typeof Service) => void;
-export declare const buildMethodDecorator: (fields: Record<string, unknown>) => (target: Service, endpointName: string) => void;
--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './decorators/generic';
-export * from './decorators/endpoints';
-export * from './service/service';
-export * from './service/serviceBuilder';
-export * from './kaqiPlaceholder';
