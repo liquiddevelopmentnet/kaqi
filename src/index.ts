2cd5f4da49aae65f0070de9b033260368c928028
Finn <info@minemix.de>
2023-12-15 21:07:30 +0100

    style: allow kaqi.placeholder usage

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export * from './decorators/endpoints'
 export * from './service/service'
 export * from './service/serviceBuilder'
 export * from './kaqiPlaceholder'
+
+import { kaqiPlaceholder } from './kaqiPlaceholder'
+
+export default { placeholder: kaqiPlaceholder }
